9bc9442131b99d4d3d89688d32e0c1e73a6cde11
MarcusACWilliams <tue79528@temple.edu>
2019-01-28 20:46:55 -0500

    node framework

---
+++ BST/BST/BST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    class BST
+    {
+        Node Root { get; set; }
+
+        int Count { get; set; }
+        int Tree_Depth { get; set; }
+
+        BST()
+        {
+            Console.WriteLine("Creating an Empty Binary Search Tree");
+        }
+
+        //Create Tree with Starting Node
+        BST(Node node)
+        {
+            Root = node;
+        }
+
+        //Insert Node into Top Node in tree
+        public void Insert(Node NewNode, Node ThatNode)
+        {
+            if(NewNode.ID > ThatNode.ID)
+            {
+
+            }
+                
+        }
+        
+        public void Remove(Node node) { }
+
+        public void Sort() { }
+
+        public void Find(int Value) { }
+
+        //Use Tree Depth to properly format print statement
+        public void PrintTree() { }
+
+       
+
+
+    }
+}
---
+++ BST/BST/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    interface INode
+    {
+
+        void PrintNode();
+    }
+
+    public class Node : INode
+    {
+        public int ID          { get; set; }
+        public Node L_child    { get; set; }
+        public Node R_child    { get; set; }
+
+        public void PrintNode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
